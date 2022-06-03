28c7c92333cb18151f6fe3b61b175edd75b8c651
ted-thuraa <mutwirited@gmail.com>
2022-06-02 20:47:03 -0700

    import modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    length = len(argv)
+    if length == 1:
+        print("0 arguments.")
+    else:
+        if length == 2:
+            print("1 argument:")
+        elif length > 2:
+            print("{} arguments".format(length - 1))
+        for i in range(1, length):
+            print("{}: {}".format(i, argv[i]))
