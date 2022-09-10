0b9a5cdfb6274d7c38e82a80f8b23dc1b5c76936
ted-thuraa <mutwirited@gmail.com>
2022-09-11 01:34:02 +0300

    cor

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-
-def getAllStates(user1, passwd1, db1):
-    """ gets states and filters as required"""
+"""main file"""
+if __name__ == '__main__':
+    import sys
     import MySQLdb
     conn = MySQLdb.connect(
-        user = user1,
-        passwd = passwd1,
-        db = db1,
-        host = "localhost",
-        port = 3306
-    )
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states WHERE `name` REGEXP BINARY '^N' 
-    ORDER BY states.id ASC""")
-
+    # HERE I have to know SQL to grab all states in my database
+    cur.execute(
+        """SELECT * FROM states WHERE `name` REGEXP BINARY '^N'
+        ORDER BY states.id ASC""")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
     cur.close()
     conn.close()
-    
-if __name__ == "__main__":
-    import sys
-    """ protected from executing when imported """
-    getAllStates(sys.argv[1], sys.argv[2], sys.argv[3])
