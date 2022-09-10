f65af97cea07b0db2dc34356a444a2c6eb8c7443
ted-thuraa <mutwirited@gmail.com>
2022-09-11 01:37:13 +0300

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,26 @@
-#!/usr/bin/python3
-"""main file"""
-if __name__ == '__main__':
-    import sys
+def getAllStates(user2, passward2, db2):
+    """ script that gets all the states when called on """
     import MySQLdb
     conn = MySQLdb.connect(
-        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+            host="localhost",
+            port=3306,
+            user=user2,
+            passwd=passward2,
+            db=db2,
+            charset="utf8")
     cur = conn.cursor()
-    # HERE I have to know SQL to grab all states in my database
     cur.execute(
-        """SELECT * FROM states WHERE `name` REGEXP BINARY '^N'
-        ORDER BY states.id ASC""")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
     cur.close()
     conn.close()
+    # engine = sqlalchemy.create_engine()
+    # db = MySQLdb.connect(host=MY_HOST, user=MY_USER, db=MY_DB)
+    # cur = db.cursor()
+
+if __name__ == "__main__":
+    import sys
+    """ protected from executing when imported """
+    getAllStates(sys.argv[1], sys.argv[2], sys.argv[3])
